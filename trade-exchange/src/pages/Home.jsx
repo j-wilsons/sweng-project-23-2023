1907e56a8a4cb87e10589f6c08d97eacea1616b3
AllanNastin <nastina@tcd.ie>
2023-02-21 00:37:54 +0000

    Router and Pages Router has been implemented and a few basic pages with no functionality

---
+++ trade-exchange/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import "../App.css";
+import logo from "../logo.svg";
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
+export const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
---
+++ trade-exchange/src/pages/Trade.jsx
@@ -0,0 +1,12 @@
+export const Trade = () => {
+    return(
+        <div>
+            <h1>Trade</h1>
+            {/* slider to buy or sell */}
+            <div>
+                <h2>Buy</h2>
+                <h2>Sell</h2>
+            </div>
+        </div>
+    );
+}
