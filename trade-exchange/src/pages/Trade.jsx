cca7a6dc2cae3be04c49aea4f279a55f32a521a3
AllanNastin <nastina@tcd.ie>
2023-02-21 09:39:38 +0000

    Simple trade page a page which sends the input to the browser dev console

--- trade-exchange/src/pages/Trade.jsx
+++ trade-exchange/src/pages/Trade.jsx
@@ -1,12 +1,54 @@
+import "../css/Main.css";
+import "../css/Text.css";
+
+var amount = 0;
+var shares = 0;
+
+const buyShares = () => {
+  amount = document.getElementById("amount").value;
+  shares = document.getElementById("shares").value;
+  console.log("Buying " + amount + " shares of " + shares);
+};
+
+const sellShares = () => {
+  amount = document.getElementById("amount").value;
+  shares = document.getElementById("shares").value;
+  console.log("Selling " + amount + " shares of " + shares);
+};
+
 export const Trade = () => {
-    return(
-        <div>
-            <h1>Trade</h1>
-            {/* slider to buy or sell */}
-            <div>
-                <h2>Buy</h2>
-                <h2>Sell</h2>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="center center-text">
+      <h1>Trade</h1>
+      <input
+        className="center-text"
+        type="text"
+        id="amount"
+        placeholder="How many shares?"
+      />
+      <input
+        className="center-text"
+        type="text"
+        id="shares"
+        placeholder="Which share?"
+      />
+      <br />
+      <button
+        className="center-text"
+        onClick={() => {
+          buyShares();
+        }}
+      >
+        Buy
+      </button>
+      <button
+        className="center-text"
+        onClick={() => {
+          sellShares();
+        }}
+      >
+        Sell
+      </button>
+    </div>
+  );
+};
