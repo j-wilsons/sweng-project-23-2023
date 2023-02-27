15bb683c5d2cadf308d6aabbb24e06b96c8d8c03
loconno5 <loconno5@tcd.ie>
2023-02-27 00:17:31 +0000

    initial layout

--- trade-exchange/src/App.js
+++ trade-exchange/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 // import "./App.css";
+import "./css/NavBar.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Trade } from "./pages/Trade";
@@ -8,7 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+      <Route index element={<Home />}/>
+        <Route exact path="/home" element={<Home />} />
         <Route exact path="/trade" element={<Trade />} />
       </Routes>
     </Router>
