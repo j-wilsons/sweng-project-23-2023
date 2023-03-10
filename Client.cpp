5d8a1cf76f7b7db83e55e801a5cb567f4da8385b
EllenClarke101 <ellclarke14@gmail.com>
2023-03-10 22:41:07 +0000

    Update Client.cpp
    User can input order and order will be formatted.

--- Client.cpp
+++ Client.cpp
@@ -9,6 +9,32 @@
 #include <string>
 #include "quickfix/SocketInitiator.h"
 
+bool isCorrectForm(string input)
+{
+    // tokenise
+    string item;
+    vector<string> items;
+    stringstream ss(input);
+    while (getline(ss, item, ' '))
+    {
+        items.push_back(item);
+    }
+
+    // checking corret formatting
+    if ((items.size() >= 3 && (items[0] == "Buy" || items[0] == "Sell") && isNum(items[1])) || items[0] == "quit")
+    {
+        return true;
+    }
+    return false;
+}
+
+string userInput(){
+    string str;
+    cout<<"Enter order: or type 'quit' to exit"<<endl;
+    getline(cin, str);              // Taking in user's order
+    return str;
+}
+
 int main()
 {
     try
@@ -19,9 +45,31 @@ int main()
         FIX::ScreenLogFactory logFactory(settings);
         FIX::SocketInitiator initiator(application, storeFactory, settings, logFactory);
         initiator.start();
-        while (true) {
-            //FIX::process_sleep(1);
+
+        do //user input
+        {
+            Order = userInput();
+            if (isCorrectForm(Order))
+            {
+                break;
+            }
+            cout << "----Order is not Correctly formatted----" << endl;
+            cout << "Please follow: Buy/Sell Quantity CompanyName or type 'quit' to exit" << endl;
+        } while (true) 
+        if(Order=="quit"){
+            return 0;
         }
+        else{
+            int OrderQuantity, Price;
+            string Symbol;                               //input: string from the client
+            stringstream ss(Order);
+            ss >> Symbol >> OrderQuantity >> Price;
+
+            Symbol = FIX::Symbol();
+            OrderQuantity = FIX::OrderQty();
+            Price = FIX::Price();
+        }
+
         return 0;
     }
     catch (FIX::ConfigError& e)
