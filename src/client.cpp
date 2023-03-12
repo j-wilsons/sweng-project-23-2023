d72d99ae2ebee2c56a74e39ba621254189cda0ca
Sultan Albogachiev <Sultanalbo06@gmail.com>
2023-03-12 21:51:49 +0000

    Taking user input and sending order
    Ellen had trouble running it on her mac so took her code, fixed and updated

--- src/client.cpp
+++ src/client.cpp
@@ -7,7 +7,46 @@
 #include "quickfix/SocketAcceptor.h"
 #include "Application.h"
 #include <string>
+#include <string.h>
 #include "quickfix/SocketInitiator.h"
+using namespace std;
+
+std::string userInput(){
+    std::string str;
+    getline(std::cin, str);              // Taking in user's order
+    return str;
+}
+
+bool isNum(string input)
+{
+    for (std::size_t i = 0; i < input.size(); i++)
+    {
+        if (isdigit(input[i]) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isCorrectForm(string input)
+{
+    // tokenise
+    string item;
+    vector<string> items;
+    stringstream ss(input);
+    while (std::getline(ss, item, ' '))
+    {
+        items.push_back(item);
+    }
+
+    // checking corret formatting
+    if (items.size() >= 3 && (items[0] == "Buy" || items[0] == "Sell") && isNum(items[1]))
+    {
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
@@ -20,11 +59,23 @@ int main()
         FIX::SocketInitiator initiator(application, storeFactory, settings, logFactory);
         initiator.start();
         while (true) {
-            char value;
-            std::cin >> value;
+            string Order;
+            do{
+                Order = userInput();
+                if (isCorrectForm(Order))
+                {
+                    break;
+                }
+                cout << "----Order is not Correctly formatted----" << endl;
+                cout << "Please follow: Buy/Sell Quantity CompanyName" << endl;
+            } while (true);
 
-            const std::string& Symbol = "TSLA";
-            int Quantity = 100;
+            string Symbol, Action;
+            int Quantity;
+
+            std::istringstream iss(Order);
+            iss >> Action >> Quantity >> Symbol;
+            
             try{
                 application.run(application.sessionId_, Symbol, Quantity);
             }catch(std::exception & e){
