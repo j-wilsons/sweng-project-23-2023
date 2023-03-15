12ee2e733666056cdee1a2a4622e1e8880601b11
AllanNastin <nastina@tcd.ie>
2023-03-15 18:25:22 +0000

    Mock Share Update Added share name to data

--- trade-exchange/src/Components/Data.js
+++ trade-exchange/src/Components/Data.js
@@ -1,7 +1,7 @@
 export const Data = [
-  { user: "all", orderComplete: 0, buy: 2, totalShares: 500, sharesLeft: 170 },
-  { user: "steve", orderComplete: 0, buy: 1, totalShares: 50, sharesLeft: 49 },
-  { user: "joe", orderComplete: 0, buy: 0, totalShares: 100, sharesLeft: 100 },
-  { user: "bob", orderComplete: 1, buy: 1, totalShares: 150, sharesLeft: 0 },
-  { user: "jesica", orderComplete: 0, buy: 1, totalShares: 200, sharesLeft: 20}
+  { user: "all", orderComplete: 0, buy: 2, share:"n/a", totalShares: 500, sharesLeft: 170 },
+  { user: "steve", orderComplete: 0, buy: 1, share:"NKE", totalShares: 50, sharesLeft: 49 },
+  { user: "joe", orderComplete: 0, buy: 0, share:"GTLB", share:"TSLA", totalShares: 100, sharesLeft: 100 },
+  { user: "bob", orderComplete: 1, buy: 1, share:"MSFT", totalShares: 150, sharesLeft: 0 },
+  { user: "jesica", orderComplete: 0, buy: 1, share:"AMZN", totalShares: 200, sharesLeft: 20}
 ];
