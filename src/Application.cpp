76a25a092c0ff8ed1996729bb93609fb7a47c9a3
Vaibhav574 <91412703+Vaibhav574@users.noreply.github.com>
2023-03-26 03:56:18 +0100

    updated Application.cpp
    made live market data run

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,7 @@
 #include "quickfix/fix44/ExecutionReport.h"
 #include <quickfix/fix44/NewOrderSingle.h>
 #include <string>
+#include "curl/curl.h"
 #include <iostream>
 #include<windows.h>           // for windows for sleeping
 void Application::onCreate(const FIX::SessionID&)
@@ -118,3 +119,66 @@ FIX44::ExecutionReport Application::tradeSuccessful(const FIX::SessionID& sessio
     std::cout << "\nTrade Successful";
     return orderReport;
 }
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+
+int Application::marketData()
+{
+    try
+    {
+
+       std::string api_key = "EPD436H91DGONRID"; // replace with your own API key
+    std::string url_base = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
+    std::vector<std::string> symbols = {"AAPL", "GOOGL", "TSLA", "AMZN"}; // replace with your desired symbols
+
+    for (const auto &symbol : symbols)
+    {
+        std::string url = url_base + symbol + "&apikey=" + api_key;
+        std::string response;
+
+        CURL *curl = curl_easy_init();
+        if (curl)
+        {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+            CURLcode res = curl_easy_perform(curl);
+            if (res != CURLE_OK)
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+            else
+                std::cout << "Response:" << std::endl
+                          << response << std::endl;
+            curl_easy_cleanup(curl);
+        }
+    }
+        
+
+        FIX::SessionSettings settings("../../src/server.cfg");
+        Application application;
+        FIX::FileStoreFactory storeFactory(settings);
+        FIX::ScreenLogFactory logFactory(settings);
+        FIX::ThreadedSocketAcceptor acceptor(application, storeFactory, settings, logFactory);
+        acceptor.start();
+        while (true) {
+     
+        }
+    
+        return 0;
+    }
+    catch (FIX::ConfigError& e)
+    { 
+        std::cout << e.what();
+        while (true) {
+        }
+        return 1;
+    }
+
+}
+
