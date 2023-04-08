ebb0e67ff711098b20b3a752801f20d4b6860a95
loconno5 <loconno5@tcd.ie>
2023-04-08 11:43:33 +0100

    adding up to date front end graphs

--- src/trade-exchange/src/pages/Trade.jsx
+++ src/trade-exchange/src/pages/Trade.jsx
@@ -3,20 +3,22 @@ import "../css/Main.css";
 import "../css/Text.css";
 import NavBar from "../Components/NavBar";
 import TradeCard from "../Components/TradeCard";
+import StockRow from "../Components/StockRow"
 // import PropTypes from "prop-types";
 import { useState } from "react";
 import { useEffect } from "react";
 import { MultiColumnComboBox } from "@progress/kendo-react-dropdowns";
 import { equities } from "../Components/Equities";
+import { orderType } from "../Components/Equities"
 import "@progress/kendo-theme-default/dist/all.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/esm/Button";
 import ApexChart from "../Components/stockChart";
 import axios from "axios";
 var amount = 0;
 var shares = 0;
+var nameO = ""
+var orderT = ""
 let myData; // variable to hold response from backend
 
 const buyShares = () => {
@@ -92,54 +94,43 @@ const columns = [
     width: "100px",
   },
 ];
-const IBMnums = [
-  33, 55, 66, 11, 22, 33, 23, 44, 66, 77, 88, 44, 55, 66, 33, 44, 55, 66, 7, 77,
-];
-const nikeNums = [
-  11, 22, 55, 11, 22, 66, 99, 44, 55, 66, 88, 33, 44, 55, 33, 22, 55, 66, 77,
-  99,
-];
-const amzNums = [
-  22, 44, 55, 66, 11, 22, 55, 88, 77, 66, 44, 33, 44, 55, 99, 77, 88, 99, 11,
-  22,
-];
-const teslaNums = [
-  77, 88, 99, 66, 55, 88, 99, 22, 33, 44, 66, 77, 88, 55, 66, 77, 44, 44, 55,
-  66,
-];
-const numLabels = [
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-];
-var dataNums = nikeNums;
+
+const orderTypes = [
+  {
+    field: "nameO",
+    header: "Order Type",
+    width: "300px"
+  }
+]
 
 export const Trade = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [isPriceShown, setIsPriceShown] = useState(false);
+  const [price, setPrice] = useState("0")
+  const [orderList, setOrderList] = useState([]);
   const [visibility, setVisibility] = useState(false);
 
+  const getInfo = event => {
+    shares = document.getElementById("shares").value;
+    setIsShown(current => !current);
+    }
+const getPrice = event => {
+  if(orderT === "Limit") {
+  setIsPriceShown(current => !current);
+}
+}
   const popupCloseHandler = (e) => {
     setVisibility(false);
   };
   const [size, setSize] = React.useState("medium");
   const [fillMode, setFillMode] = React.useState("solid");
   const [rounded, setRounded] = React.useState("small");
+
+  const updatePrice = price => {
+    this.setState({
+      price: this.state.price
+    })
+  }
 
   useEffect(() => {
     const element = document.querySelector("#EqList");
@@ -149,19 +140,19 @@ export const Trade = () => {
   return (
     <div className="home">
       <NavBar />
-      <div className="row2">
+      <div className="right-pos">
         <div>
           <h2>MAKE A TRADE</h2>
           <div>
             <input
-              className="center-text"
               type="text"
               id="amount"
-              placeholder="Select amount ..."
+              placeholder="Select quantity ..."
               rounded={rounded}
               style={{
-                width: "293px",
+                width: "300px",
                 height: "27px",
+                fontSize: 15
               }}
             />
             <div
@@ -175,17 +166,74 @@ export const Trade = () => {
                 }}
                 data={equities}
                 columns={columns}
-                textField={"name"}
+                textField={"symbol"}
                 size={size}
                 fillMode={fillMode}
                 rounded={rounded}
                 id="shares"
                 placeholder="Select share ..."
+                
               />
             </div>
-
+            <div 
+    style={{
+      
+      justifyContent: "space-around"
+    }}>
+      <MultiColumnComboBox
+      
+        style={{
+          width: "300px",
+        }}
+        data={orderType}
+        columns={orderTypes}
+        textField={"nameO"}
+        size={size}
+          fillMode={fillMode}
+          rounded={rounded}
+          id="nameO"
+          orderT={nameO}
+        placeholder="Select type ..."
+        onChange={() => {
+          orderT = document.getElementById("nameO").value
+          console.log("OT " + orderT)
+          getPrice()
+         }}
+        
+      />
+     </div> 
+     {isPriceShown ?
+     <div>
+     <input
+        type="text"
+        id="price"
+        placeholder="Select price ..."
+        rounded={rounded}
+        price={price}
+        style={{
+          width: "300px",
+          height: "27px",
+          fontSize: 15
+          
+        }}
+      />
+    </div>
+: null}
             <br />
-            <div className="row2">
+            <div className="row2" 
+            style={{ marginLeft: -2}}>
+            <button
+        className="buyButton" 
+        variant="primary" 
+        size="lg" 
+        style={{backgroundColor: 'yellow'}}
+        onClick={() => {
+          getInfo();
+          
+        }}
+      >
+        Info
+      </button>
               <button
                 className="buyButton"
                 variant="primary"
@@ -210,28 +258,36 @@ export const Trade = () => {
           </div>
         </div>
 
-        <div className="graph">
-          <ApexChart
-            data={dataNums}
-            data2={IBMnums}
-            data3={amzNums}
-            data4={teslaNums}
-            dLabels={numLabels}
-          ></ApexChart>
-        </div>
-        <div className="right-pos">
-          <Container fluid>
-            <Row>
-              <Col className="container-pos">Current equities</Col>
-              <Col className="container-pos">No. Shares</Col>
-            </Row>
-            <Row>
-              <Col className="container-pos">shares</Col>
-              <Col className="container-pos">amount</Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
+      
+      
+        {isShown ? (
+       <div className="left-pos" >
+       <div className="container" >
+         <table className="table mt-3" style={{color: 'white'}}>
+           <thead>
+             <tr>
+               <th>Ticker</th>
+               <th>Price</th>
+               <th>Change</th>
+               <th>Date</th>
+             </tr>
+           </thead>
+           <tbody>
+             <StockRow ticker={shares} />
+           </tbody>
+         </table>
+       </div>
+     
+       </div>
+    ): null }
+    </div> 
+   
+      {visibility ? (
+         <div>
+      <TradeCard onClose={popupCloseHandler}
+        show={visibility} title={Trade} />
+          </div>
+          ): null }
     </div>
   );
 };
